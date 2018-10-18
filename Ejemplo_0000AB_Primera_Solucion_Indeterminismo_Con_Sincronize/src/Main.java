216285c9078bef32a2d62911cad38e95d24efc4d
PedroProfe <44172877+PedroProfe@users.noreply.github.com>
2018-10-18 21:53:01 +0200

    Add files via upload

---
+++ Ejemplo_0000AB_Primera_Solucion_Indeterminismo_Con_Sincronize/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				Thread[] hilos = new Thread[1000];
+
+				for (int i=0;i <hilos.length; i++){
+
+					hilos[i] = new Thread(new RHilo());
+					hilos[i].start();
+				}
+
+				/*El problema es que se va a ejecutar la linea de abajo cuando todabía se están
+				 * ejecutando los hilos que lanzó...además incluso aunque lo solucionemos con
+				 * un join...los hilos podrían acceder al mismo tiempo a la variable cont*/
+
+				/*Solución parcial, sigue sin funcionar incluso con el for de abajo*/
+				for (int i=0;i <hilos.length; i++){
+					try {
+						hilos[i].join();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				/*la razón por la cual ni con el join funcione es por el indeterminismo y esto
+				ocurre cuando dos o más hilos intentan escribir en una variable compartida
+				se produce indeterminación, ¿Por qué ocurre est? porque si 3 hilos accedieron
+				a la vez y leyeron la variable a la vez con un valor por ejemplo 100, al incrementar
+				el valor escribirán 101 y no lo que correspondería "103". */
+				System.out.println("El valor de cont es "+ RHilo.getCont());
+	}
+
+}
